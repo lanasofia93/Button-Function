1957b12e101002d0b55ff39fe739b127e38aafef
lanasofia93 <60903344+lanasofia93@users.noreply.github.com>
2021-05-28 17:25:17 -0300

    Add files via upload

---
+++ estilo.js
@@ -0,0 +1,16 @@
+alert ("Seja bem vindo");
+
+function clicou (){
+    alert ("Clicou no botão")
+}
+
+function passouMouse (){
+    alert ("Passou o mouse")
+    }
+
+function mousepressionado (){
+    alert ("Bem vindo");
+    }
+function duploclique (){
+alert("Você deu um duplo clique");
+}
